4e57c9468fc29ef87ed12c49b28d80bcdbddce01
Sachin-Dhuriya <sachindhuriya561@gmail.com>
2026-01-06 15:56:25 +0530

    Profile API implimented

---
+++ auth-service/src/controllers/profile.controller.ts
@@ -0,0 +1,41 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import { env } from "../config/env";
+import { User } from "../models/user.model";
+import { failedResponseHandler } from "../utils/failedResponse";
+import { successResponseHandler } from "../utils/successResponse";
+import { UserStatus } from "../utils/userRole";
+
+interface JwtPayload {
+  userId: number;
+  role: string;
+}
+
+export async function profile(req: Request, res: Response, next: NextFunction) {
+  try {
+    const header = req.headers.authorization;
+    if (!header) {
+      return failedResponseHandler(res, 401, "Please login first");
+    }
+    if (!header.startsWith("Bearer ")) {
+      return failedResponseHandler(res, 401, "Invalid authorization format");
+    }
+
+    const token = header.split(" ")[1];
+
+    const decode = (await jwt.verify(token, env.JWT_SECRET)) as JwtPayload;
+
+    const user = await User.findOne({
+      where: { id: decode.userId, status: UserStatus.ACTIVE },
+      raw: true,
+      attributes: ["name", "email", "status"],
+    });
+    if (!user) {
+      return failedResponseHandler(res, 404, "User not found");
+    }
+
+    return successResponseHandler(res, 200, `Welcome ${user.name}`, user);
+  } catch (error) {
+    next(error);
+  }
+}
--- auth-service/src/routes/router.ts
+++ auth-service/src/routes/router.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { signup } from "../controllers/signup.controller";
 import { login } from "../controllers/login.controller";
+import { profile } from "../controllers/profile.controller";
 const router = Router();
 
 router.post("/signup", signup);
-router.post("/login", login)
+router.post("/login", login);
+router.get("/profile", profile)
 
 export default router;
